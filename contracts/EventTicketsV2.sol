fd8f5706e014f6de50c249c4bb5aaf0ccc5beecb
Joonbo Rhie <jb.rhie@gmail.com>
2020-01-24 13:20:22 -0500

    Updated idGenerator

--- contracts/EventTicketsV2.sol
+++ contracts/EventTicketsV2.sol
@@ -81,7 +81,7 @@ contract EventTicketsV2 {
         events[eventId].website = _website;
         events[eventId].totalTickets = _tickets;
         events[eventId].isOpen = true;
-        eventId = idGenerator;
+        idGenerator = _eventId;
         idGenerator = idGenerator + 1;
         emit LogEventAdded(_description, _website, _tickets, eventId);
         return(eventId);
